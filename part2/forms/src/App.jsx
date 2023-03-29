09d6db85880a36bed12fc065678599fda14a6878
Steven <s.spencerletts@gmail.com>
2023-03-29 15:42:53 +0100

    add person notification component with style

--- part2/forms/src/App.jsx
+++ part2/forms/src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNum, setNewNum] = useState("");
   const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     PersonService.getAll().then((response) => setPersons(response.data));
@@ -41,6 +42,8 @@ const App = () => {
       setPersons(persons.concat(response.data));
       setNewName("");
       setNewNum("");
+      setMessage(`Added ${newPerson.name}`);
+      setTimeout(() => setMessage(""), 5000);
     });
   };
 
@@ -66,6 +69,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification m={message} />
       <Filter onChange={handleSearchChange} value={search} />
       <h3>Add a New</h3>
       <PersonForm
@@ -95,3 +99,24 @@ const checkNewName = (arr, name) => {
 };
 
 export default App;
+
+const Notification = ({ m }) => {
+  const styles = {
+    color: "green",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  };
+  if (m) {
+    return (
+      <div className="confirm" style={styles}>
+        {m}
+      </div>
+    );
+  } else {
+    return null;
+  }
+};
