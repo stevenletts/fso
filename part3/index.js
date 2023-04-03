39c90a78c546bde4e3908b716940142a6aecc507
Steven <s.spencerletts@gmail.com>
2023-04-03 11:46:14 +0100

    production build of frontend added

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
+app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(
   morgan((tokens, req, res) => {
@@ -97,7 +100,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
