d461aa8977438e846123e4f65cadc353be93921b
Steven <s.spencerletts@gmail.com>
2023-04-10 14:44:00 +0100

    update and delete functionality added and tests for routes using supertest package complete

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,23 +1,46 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
+blogRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogRouter.post("/", (request, response, next) => {
-  const blog = new Blog(request.body);
-  // add formatting for Blog object
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
+blogRouter.post("/", async (request, response) => {
+  const body = request.body;
+
+  if (!body.likes) {
+    body.likes = 0;
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+});
+
+blogRouter.delete("/:id", async (request, response) => {
+  const deleted = await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response) => {
+  const body = request.body;
+  const updated = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, updated, {
+    new: true,
+  });
+  response.status(200);
+  response.json(updatedBlog);
 });
 
 module.exports = blogRouter;
---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,139 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+const Blog = require("../models/blog");
+const { log } = require("console");
+const api = supertest(app);
+
+const startingBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+];
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  let blogObj = new Blog(startingBlogs[0]);
+  await blogObj.save();
+  blogObj = new Blog(startingBlogs[1]);
+  await blogObj.save();
+});
+
+test("blogs returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+
+test("all blogs returned", async () => {
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(2);
+});
+
+test("unique identifier", async () => {
+  const blogs = await Blog.find({});
+  expect(blogs[0].id).toBeDefined();
+});
+
+test("blog post", async () => {
+  const newBlog = startingBlogs[2];
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const blogsAfter = await Blog.find({});
+  expect(blogsAfter).toHaveLength(3);
+  const content = blogsAfter.map((blog) => blog.title);
+  expect(content).toContain("Canonical string reduction");
+});
+
+test("zero likes", async () => {
+  const newBlog = {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    __v: 0,
+  };
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+  const blogsAfter = await Blog.find({});
+  const content = blogsAfter.map((blog) => blog.likes);
+  expect(content).toContain(0);
+});
+
+test("delete", async () => {
+  const deleted = await api.delete("/api/blogs/5a422aa71b54a676234d17f8");
+  const blogsAfter = await Blog.find({});
+  const content = blogsAfter.map((blog) => blog.title);
+  expect(content).toHaveLength(1);
+  expect(content).not.toContain("Go To Statement Considered Harmful");
+});
+
+test("update", async () => {
+  const updater = startingBlogs[0];
+  updater.likes = updater.likes + 1;
+  const updated = await api
+    .put("/api/blogs/5a422aa71b54a676234d17f8")
+    .send(updater)
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+  const blog = await Blog.findById({
+    _id: "5a422aa71b54a676234d17f8",
+  });
+  expect(blog.likes).toBe(8);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
