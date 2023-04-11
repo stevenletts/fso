d8ca69ba0b91593f8d7ee92561364dc67a2b8d61
Steven <s.spencerletts@gmail.com>
2023-04-11 15:55:53 +0100

    login backend and token verification added, and users model, routing complete

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,35 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFromRequest = (request) => {
+  const auth = request.get("authorization");
+
+  if (auth && auth.startsWith("bearer ")) {
+    return auth.replace("bearer ", "");
+  }
+  return null;
+};
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
+
+  const decodedToken = jwt.verify(
+    getTokenFromRequest(request),
+    process.env.SECRET
+  );
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token error" });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   if (!body.likes) {
     body.likes = 0;
@@ -18,8 +40,13 @@ blogRouter.post("/", async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user.id,
   });
+
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blog");
 const { log } = require("console");
+const User = require("../models/user");
 const api = supertest(app);
 
 const startingBlogs = [
@@ -55,83 +57,139 @@ const startingBlogs = [
     __v: 0,
   },
 ];
-
-beforeEach(async () => {
-  await Blog.deleteMany({});
-  let blogObj = new Blog(startingBlogs[0]);
-  await blogObj.save();
-  blogObj = new Blog(startingBlogs[1]);
-  await blogObj.save();
-});
-
-test("blogs returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
-test("all blogs returned", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(2);
-});
-
-test("unique identifier", async () => {
-  const blogs = await Blog.find({});
-  expect(blogs[0].id).toBeDefined();
-});
-
-test("blog post", async () => {
-  const newBlog = startingBlogs[2];
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-  const blogsAfter = await Blog.find({});
-  expect(blogsAfter).toHaveLength(3);
-  const content = blogsAfter.map((blog) => blog.title);
-  expect(content).toContain("Canonical string reduction");
-});
-
-test("zero likes", async () => {
-  const newBlog = {
-    _id: "5a422bc61b54a676234d17fc",
-    title: "Type wars",
-    author: "Robert C. Martin",
-    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-    __v: 0,
-  };
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-  const blogsAfter = await Blog.find({});
-  const content = blogsAfter.map((blog) => blog.likes);
-  expect(content).toContain(0);
-});
-
-test("delete", async () => {
-  const deleted = await api.delete("/api/blogs/5a422aa71b54a676234d17f8");
-  const blogsAfter = await Blog.find({});
-  const content = blogsAfter.map((blog) => blog.title);
-  expect(content).toHaveLength(1);
-  expect(content).not.toContain("Go To Statement Considered Harmful");
-});
-
-test("update", async () => {
-  const updater = startingBlogs[0];
-  updater.likes = updater.likes + 1;
-  const updated = await api
-    .put("/api/blogs/5a422aa71b54a676234d17f8")
-    .send(updater)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-  const blog = await Blog.findById({
-    _id: "5a422aa71b54a676234d17f8",
+describe("blogs functionality", () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({});
+    let blogObj = new Blog(startingBlogs[0]);
+    await blogObj.save();
+    blogObj = new Blog(startingBlogs[1]);
+    await blogObj.save();
   });
-  expect(blog.likes).toBe(8);
+
+  test("blogs returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs returned", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(2);
+  });
+
+  test("unique identifier", async () => {
+    const blogs = await Blog.find({});
+    expect(blogs[0].id).toBeDefined();
+  });
+
+  test("blog post", async () => {
+    const newBlog = startingBlogs[2];
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+    const blogsAfter = await Blog.find({});
+    expect(blogsAfter).toHaveLength(3);
+    const content = blogsAfter.map((blog) => blog.title);
+    expect(content).toContain("Canonical string reduction");
+  });
+
+  test("zero likes", async () => {
+    const newBlog = {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      __v: 0,
+    };
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+    const blogsAfter = await Blog.find({});
+    const content = blogsAfter.map((blog) => blog.likes);
+    expect(content).toContain(0);
+  });
+
+  test("delete", async () => {
+    const deleted = await api.delete("/api/blogs/5a422aa71b54a676234d17f8");
+    const blogsAfter = await Blog.find({});
+    const content = blogsAfter.map((blog) => blog.title);
+    expect(content).toHaveLength(1);
+    expect(content).not.toContain("Go To Statement Considered Harmful");
+  });
+
+  test("update", async () => {
+    const updater = startingBlogs[0];
+    updater.likes = updater.likes + 1;
+    const updated = await api
+      .put("/api/blogs/5a422aa71b54a676234d17f8")
+      .send(updater)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    const blog = await Blog.findById({
+      _id: "5a422aa71b54a676234d17f8",
+    });
+    expect(blog.likes).toBe(8);
+  });
+});
+
+describe("users api tests", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+
+    const passwordHash = await bcrypt.hash("sekret", 10);
+    const user = new User({ username: "root", passwordHash });
+    await user.save();
+  });
+
+  test("new user created", async () => {
+    const usersAtStart = await User.find({});
+    const userAtStartList = usersAtStart.map((u) => u.toJSON());
+
+    const newUser = {
+      username: "steven",
+      name: "Steven",
+      password: "password",
+    };
+
+    await api
+      .post("/api/users")
+      .send(newUser)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const usersAtEnd = await User.find({});
+    const usersAtEndList = usersAtEnd.map((u) => u.toJSON);
+
+    expect(usersAtEndList).toHaveLength(userAtStartList.length + 1);
+
+    const usernames = usersAtEnd.map((u) => u.username);
+    expect(usernames).toContain(newUser.username);
+  });
+
+  test("invalid username rejected", async () => {
+    const newUser = {
+      username: "st",
+      name: "Steven",
+      password: "password",
+    };
+
+    await api.post("/api/users").send(newUser).expect(500);
+  });
+
+  test("invalid password", async () => {
+    const newUser = {
+      username: "steven",
+      name: "Steven",
+      password: "a",
+    };
+
+    await api.post("/api/users").send(newUser).expect(400);
+  });
 });
 
 afterAll(async () => {
