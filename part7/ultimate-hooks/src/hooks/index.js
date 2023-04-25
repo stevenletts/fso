7a763a6ae1aef9571e8b0449b1b1117c31d927ac
Steven <s.spencerletts@gmail.com>
2023-04-25 15:40:07 +0100

    custom hook for server communication

---
+++ part7/ultimate-hooks/src/hooks/index.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([]);
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl);
+    setResources(response.data);
+  };
+
+  const create = async (object) => {
+    const response = await axios.post(baseUrl, object);
+    setResources([...resources, response.data]);
+  };
+
+  const services = {
+    getAll,
+    create,
+  };
+
+  return [resources, services];
+};
