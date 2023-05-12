0c2fe41ef31ae1c1fe9497828e5d4af79729a59e
Steven <s.spencerletts@gmail.com>
2023-05-12 06:26:18 +0100

    refactoring

--- part8/backend/resolvers.js
+++ part8/backend/resolvers.js
@@ -1,30 +1,3 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const { GraphQLError } = require("graphql");
-const jwt = require("jsonwebtoken");
-
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const Book = require("./models/book");
-const Author = require("./models/author");
-const User = require("./models/user");
-
-require("dotenv").config();
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-console.log("connecting to ", MONGODB_URI);
-
-mongoose
-  .connect(MONGODB_URI)
-  .then(() => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB: ", error.message);
-  });
-
-
 const resolvers = {
   Query: {
     bookCount: async () => Book.collection.countDocuments(),
@@ -142,26 +115,3 @@ const resolvers = {
     },
   },
 };
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorisaztion : null;
-    
-    if (auth && auth.startsWith("Bearer ")) {
-      const decodedToken = jwt.verify(
-        auth.substring(7),
-        process.env.JWT_SECRET
-      );
-      const currentUser = await User.findById(decodedToken.id);
-      return { currentUser };
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
---
+++ part8/backend/typeDefs.js
@@ -0,0 +1,48 @@
+const typeDefs = `
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+    me: User
+  }
+
+  type User {
+    username: String!
+    favouriteGenre: String! 
+    id: ID!
+  }
+
+  type Token {
+    value: String! 
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: Author!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+    editAuthor(name: String!, year: Int!): Author
+    createUser(username: String!, favouriteGenre: String!): User
+    login(username: String!, password: String!)
+ : Token  
+}`;
+
+module.exports = typeDefs;
