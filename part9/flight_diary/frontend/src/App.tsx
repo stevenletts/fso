e3ec8381d3b08b114cfd584df721217c1a2d2a9d
Steven <s.spencerletts@gmail.com>
2023-05-20 08:30:03 +0100

    new diary form implemented

--- part9/flight_diary/frontend/src/App.tsx
+++ part9/flight_diary/frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import { DiaryEntry } from "./types";
 import Diaries from "./components/Diaries";
+import NewDiary from "./components/NewDiary";
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/diaries/").then((response) => {
@@ -13,8 +15,24 @@ function App() {
     });
   }, []);
 
+  const addEntry = (entry: object): null => {
+    axios
+      .post<DiaryEntry>("http://localhost:3000/api/diaries/", entry)
+      .then((response) => {
+        setDiaries(diaries.concat(response.data));
+      })
+      .catch((error) => {
+        setError(error.response.data);
+        setTimeout(() => setError(""), 5000);
+      });
+
+    return null;
+  };
+
   return (
     <div>
+      {error ? <h1>{error}</h1> : null}
+      <NewDiary addEntry={addEntry} />
       <Diaries diaries={diaries} />
     </div>
   );
--- part9/flight_diary/frontend/src/components/NewDiary.tsx
+++ part9/flight_diary/frontend/src/components/NewDiary.tsx
@@ -1,9 +1,85 @@
-const NewDiary = () => {
-    return <div>
-        <form>
-            
-        </form>
-    </div>
-}
+import { useState } from "react";
 
-export default NewDiary
+const NewDiary = ({
+  addEntry,
+}: {
+  addEntry: (entry: object) => void;
+}): JSX.Element => {
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
+
+  const visibilityValues: string[] = ["great", "good", "ok", "poor"];
+  const weatherValues: string[] = [
+    "sunny",
+    "rainy",
+    "cloudy",
+    "stormy",
+    "windy",
+  ];
+
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    addEntry({ date, visibility, weather, comment });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+    setComment("");
+  };
+  return (
+    <div>
+      <h2>Add a new Entry</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Date: </label>
+          <input
+            type="date"
+            onChange={({ target }) => setDate(target.value)}
+            value={date}
+          />
+          <div>
+            {visibilityValues.map((value, i) => (
+              <span key={i}>
+                <input
+                  type="radio"
+                  name="visibility"
+                  onChange={() => setVisibility(value)}
+                  value={visibility}
+                  checked={visibility === value}
+                />
+                <label>{value}</label>
+              </span>
+            ))}
+          </div>
+          <div>
+            {weatherValues.map((value, i) => (
+              <span key={i}>
+                <input
+                  type="radio"
+                  name="weather"
+                  onChange={() => setWeather(value)}
+                  value={weather}
+                  checked={visibility === value}
+                />
+                <label>{value}</label>
+              </span>
+            ))}
+          </div>
+
+          <div>
+            <label>Comment: </label>
+            <input
+              type="text"
+              onChange={({ target }) => setComment(target.value)}
+              value={comment}
+            ></input>
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewDiary;
